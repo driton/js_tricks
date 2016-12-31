4cf26b1c57f2cc629b27d6636dfddfc92011f1b0
Dritoni <dritoni.h@gmail.com>
2016-12-31 14:02:53 +0100

    Changed

--- asObject.js
+++ asObject.js
@@ -57,7 +57,7 @@ function executeThis(param){
 	}
 }
 
-var e = executeThis('("hello") ? {1: "Verheiratet", "hello" : "This is my Name" , 3 : "In eine Beziehung", default: "Unknown"}');
+var e = executeThis('("hello") ? {1: "Verheiratet", "hello" : "Geschiden" , 3 : "In eine Beziehung", default: "Unknown"}');
 console.log(eval(e));
 
 
